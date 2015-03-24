eff2c82322dc1d75d6ca7c5728937ba38e1f0c21
taivo <taivo@bitweb.ee>
2015-03-24 15:16:51 +0200

    Removed unnecessary constant.

--- src/main/java/ee/bitweb/ogone/Response.java
+++ src/main/java/ee/bitweb/ogone/Response.java
@@ -2,7 +2,6 @@ package ee.bitweb.ogone;
 
 public interface Response {
 
-    public static final String HASIGN_FIELD = "SHASIGN";
     public boolean isSuccessful();
     public Object getParameter(String key);
 
